7dab1c7240f8b4b00e91205d39ff51a90330617b
waiwai <857557118@qq.com>
2025-08-27 19:03:01 +0800

    🔥 remove tvshow useless fileds

--- src/main/java/com/uwetrottmann/tmdb2/entities/BaseTvShow.java
+++ src/main/java/com/uwetrottmann/tmdb2/entities/BaseTvShow.java
@@ -11,7 +11,7 @@ public class BaseTvShow extends BaseRatingObject {
     public String overview;
     public String name;
     public List<String> origin_country;
-    public List<Integer> genre_ids;
+    //public List<Integer> genre_ids;
     public Date first_air_date;
     public String backdrop_path;
     public String poster_path;
