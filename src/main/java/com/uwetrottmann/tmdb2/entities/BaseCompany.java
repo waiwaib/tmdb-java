b230fa4fad91572fb51a225288e3890a1bc88be5
waiwai <857557118@qq.com>
2025-08-28 15:45:46 +0800

    🔥 remove Movie useless fileds

--- src/main/java/com/uwetrottmann/tmdb2/entities/BaseCompany.java
+++ src/main/java/com/uwetrottmann/tmdb2/entities/BaseCompany.java
@@ -5,5 +5,5 @@ public class BaseCompany {
     public Integer id;
     //public String name;
     //public String logo_path;
-    //public String origin_country;
+    public String origin_country;
 }
--- src/main/java/com/uwetrottmann/tmdb2/entities/ReleaseCountry.java
+++ src/main/java/com/uwetrottmann/tmdb2/entities/ReleaseCountry.java
@@ -2,7 +2,7 @@ package com.uwetrottmann.tmdb2.entities;
 
 public class ReleaseCountry {
     public String iso_3166_1;
-    public String primary;
+   // public String primary;
     public String release_date;
     public String certification;
 }
